98a0fff57c549968a506be177b89c0401c437d39
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-28 22:09:44 +0600

    some changes

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -21,9 +21,9 @@ public class MainApp {
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
       Car car = new Car(5,"BMW");
-      userService.add(new User("User5", "Lastname5", "user5@gmail.ru",car));
-      userService.add(new User("User6", "Lastname6", "user6@gmail.ru",new Car(5,"Chevrolet")));
-      userService.add(new User("User7", "Lastname7", "user7@gmail.ru",new Car(8,"KIA")));
+      userService.add(new User("User5", "Lastname5", "user5@gmail.ru", car));
+      userService.add(new User("User6", "Lastname6", "user6@gmail.ru", new Car(5,"Chevrolet")));
+//      userService.add(new User("User7", "Lastname7", "user7@gmail.ru", new Car(8,"KIA")));
 
 
       List<User> users = userService.listUsers();
